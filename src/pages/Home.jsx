819011e5909b16442d79d9bf6c4d42e81f97b14f
vishalJ-addweb <vishal.j@addwebsolution.in>
2021-12-29 18:55:26 +0530

    fix : size of div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box } from "@mui/material";
 
 const Home = () => {
-  return <Box>This is Home Page</Box>;
+  return <Box sx={{ backgroundColor: "red" }}>This is Home Page</Box>;
 };
 
 export default Home;
