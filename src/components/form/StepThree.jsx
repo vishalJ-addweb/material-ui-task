b0a9555ff03c98a040fe31fcfe61df6a2fe3d693
vishalJ-addweb <vishal.j@addwebsolution.in>
2022-01-12 18:41:28 +0530

    fix : validation

--- src/components/form/StepThree.jsx
+++ src/components/form/StepThree.jsx
@@ -1,36 +1,52 @@
 import React from "react";
-import { Box, Autocomplete, TextField } from "@mui/material";
+import { Box, Autocomplete, TextField, Typography } from "@mui/material";
 import { useFormContext, Controller } from "react-hook-form";
 import { chip } from "../../utils/DropDownItems";
-import styles from "../../css/Forms.module.css";
 
-const StepThree = () => {
+const StepThree = (props) => {
   const {
     control,
     formState: { errors },
   } = useFormContext();
   return (
-    <Box className={styles.box}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+      }}
+    >
+      <Typography sx={{ fontWeight: "bold", fontSize: "25px" }}>
+        Health and Fitness
+      </Typography>
       <Controller
         name="chip"
         control={control}
         rules={{
-          validate: (value) => value.length >= 3,
+          required: "Field is Required",
+          validate: (value) => value.length >= 3 || "Select at least 3 values",
         }}
         render={({ field: { onChange } }) => (
           <Autocomplete
             multiple
-            sx={{ width: 300 }}
+            sx={{ width: 390 }}
             onChange={(event, data) => onChange(data)}
             options={chip}
+            // defaultValue={[chip[0], chip[1], chip[2]]}
+            defaultValue={[
+              props.defaultChip[0],
+              props.defaultChip[1],
+              props.defaultChip[2],
+            ]}
             getOptionLabel={(option) => (option ? option.label : "")}
             renderInput={(params) => (
               <TextField
+                sx={{ margin: "10px" }}
                 {...params}
                 label="Chip"
                 placeholder="Favorites"
                 error={Boolean(errors?.chip)}
-                helperText={errors.chip ? "Put Atleast Three Values" : null}
+                helperText={errors.chip?.message}
               />
             )}
           />
