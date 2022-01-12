7310774740039a9d513afeb6a4718d209c8d536a
vishalJ-addweb <vishal.j@addwebsolution.in>
2022-01-12 18:41:09 +0530

    fix : validation

--- src/components/form/StepOne.jsx
+++ src/components/form/StepOne.jsx
@@ -8,33 +8,17 @@ import {
   FormControlLabel,
   Radio,
   Checkbox,
+  Typography,
 } from "@mui/material";
-import styles from "../../css/Forms.module.css";
 import { useFormContext, Controller } from "react-hook-form";
-
-const checkBoxItems = [
-  {
-    id: 0,
-    name: "Car",
-  },
-  {
-    id: 1,
-    name: "MotorBike",
-  },
-  {
-    id: 2,
-    name: "Bicycle",
-  },
-  {
-    id: 3,
-    name: "Boat",
-  },
-];
+import { checkBoxItems } from "../../utils/DropDownItems";
 
 const StepOne = (props) => {
   const {
     control,
     getValues,
+    setError,
+    clearErrors,
     formState: { errors },
   } = useFormContext();
   const handleChecked = (checked) => {
@@ -45,146 +29,186 @@ const StepOne = (props) => {
     return newIds;
   };
   return (
-    <Box className={styles.box}>
-      <Controller
-        control={control}
-        name="firstName"
-        rules={{
-          required: "First Name is required.",
-          minLength: { value: 4, message: "Minimum Length is 4" },
-          maxLength: { value: 20, message: "Minimum Length is 20" },
+    <>
+      <Typography sx={{ fontWeight: "bold", fontSize: "25px" }}>
+        Personal Details
+      </Typography>
+      <Box
+        // className={styles.box}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
         }}
-        render={({ field }) => (
-          <TextField
-            id="outlined-basic"
-            label="First-Name"
-            variant="outlined"
-            {...field}
-            error={Boolean(errors?.firstName)}
-            helperText={errors.firstName?.message}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="lastName"
-        render={({ field }) => (
-          <TextField
-            id="outlined-basic"
-            label="Last-Name"
-            variant="outlined"
-            {...field}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="email"
-        rules={{
-          required: "Email is required.",
-        }}
-        render={({ field }) => (
-          <TextField
-            id="outlined-basic"
-            label="Email"
-            variant="outlined"
-            {...field}
-            error={Boolean(errors?.email)}
-            helperText={errors.email?.message}
-            type="email"
-          />
-        )}
-      />
-
-      <Controller
-        control={control}
-        name="age"
-        render={({ field }) => (
-          <TextField
-            id="outlined-basic"
-            label="Age"
-            variant="outlined"
-            {...field}
-          />
-        )}
-      />
-
-      <FormControl component="fieldset">
-        <FormLabel component="legend">Gender</FormLabel>
-        <Controller
-          control={control}
-          name="gender"
-          rules={{ required: "Gender is required field" }}
-          render={({ field }) => (
-            <RadioGroup {...field} name="radio-buttons-group">
-              <FormControlLabel value="male" control={<Radio />} label="Male" />
-              <FormControlLabel
-                value="female"
-                control={<Radio />}
-                label="Female"
+      >
+        <Box sx={{ margin: "5px" }}>
+          <Controller
+            control={control}
+            name="firstName"
+            rules={{ required: "First Name is required." }}
+            render={({ field }) => (
+              <TextField
+                sx={{ width: "49%" }}
+                onChange={(e) => {
+                  if (e.target.value.length < 4) {
+                    setError("firstName", {
+                      type: "validate",
+                      message: "Minimum 4 Letters required in First-Name",
+                    });
+                    field.onChange(e);
+                  } else {
+                    field.onChange(e);
+                    clearErrors("firstName");
+                  }
+                }}
+                id="outlined-basic"
+                label="First-Name"
+                variant="outlined"
+                // {...field}
+                value={field.value}
+                error={Boolean(errors?.firstName)}
+                helperText={errors.firstName?.message}
               />
-              <FormControlLabel
-                value="other"
-                control={<Radio />}
-                label="Other"
-              />
-            </RadioGroup>
-          )}
-        />
-      </FormControl>
-      {errors.gender ? (
-        <p
-          style={{
-            color: "red",
-            margin: "0px",
-            padding: "0px 0px 0px 10px",
-            fontSize: "14px",
-          }}
-        >
-          {errors.gender.message}
-        </p>
-      ) : null}
+            )}
+          />
 
-      <FormControl>
-        <FormLabel component="legend">Mode Of Transportation</FormLabel>
-        <Controller
-          name="modes"
-          control={control}
-          rules={{
-            validate: (value) => value.length >= 2,
-          }}
-          render={({ field }) =>
-            checkBoxItems.map((item) => (
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    onChange={() => field.onChange(handleChecked(item.name))}
-                    defaultChecked={
-                      props.defaultChecked.includes(item.name) ? true : false
-                    }
-                  />
-                }
-                key={item.id}
-                label={item.name}
+          <Controller
+            control={control}
+            name="lastName"
+            render={({ field }) => (
+              <TextField
+                sx={{ width: "49%", marginLeft: "20px" }}
+                id="outlined-basic"
+                label="Last-Name"
+                variant="outlined"
                 {...field}
               />
-            ))
-          }
-        />
-      </FormControl>
-      {errors.modes ? (
-        <p
-          style={{
-            color: "red",
-            margin: "0px",
-            padding: "0px 0px 0px 10px",
-            fontSize: "14px",
-          }}
+            )}
+          />
+        </Box>
+
+        <Box sx={{ margin: "5px" }}>
+          <Controller
+            control={control}
+            name="email"
+            rules={{
+              required: "Email is required.",
+            }}
+            render={({ field }) => (
+              <TextField
+                sx={{ width: "49%" }}
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                {...field}
+                error={Boolean(errors?.email)}
+                helperText={errors.email?.message}
+                type="email"
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="age"
+            render={({ field }) => (
+              <TextField
+                sx={{ width: "49%", marginLeft: "20px" }}
+                id="outlined-basic"
+                label="Age"
+                variant="outlined"
+                {...field}
+              />
+            )}
+          />
+        </Box>
+
+        <FormControl
+          component="fieldset"
+          sx={{ width: "100px", margin: "5px" }}
         >
-          {errors.modes.type === "validate" ? "Select Minimum Two Mode" : null}
-        </p>
-      ) : null}
-    </Box>
+          <FormLabel component="legend">Gender</FormLabel>
+          <Controller
+            control={control}
+            name="gender"
+            rules={{ required: "Gender is required field" }}
+            render={({ field }) => (
+              <RadioGroup {...field} name="radio-buttons-group">
+                <FormControlLabel
+                  value="male"
+                  control={<Radio />}
+                  label="Male"
+                />
+                <FormControlLabel
+                  value="female"
+                  control={<Radio />}
+                  label="Female"
+                />
+                <FormControlLabel
+                  value="other"
+                  control={<Radio />}
+                  label="Other"
+                />
+              </RadioGroup>
+            )}
+          />
+        </FormControl>
+        {errors.gender ? (
+          <p
+            style={{
+              color: "red",
+              margin: "0px",
+              padding: "0px 0px 0px 10px",
+              fontSize: "14px",
+            }}
+          >
+            {errors.gender.message}
+          </p>
+        ) : null}
+
+        <FormControl sx={{ width: "200px", margin: "5px" }}>
+          <FormLabel component="legend">Mode Of Transportation</FormLabel>
+          <Controller
+            name="modes"
+            control={control}
+            rules={{
+              validate: (value) => value.length >= 2,
+            }}
+            render={({ field }) =>
+              checkBoxItems.map((item) => (
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      onChange={() => field.onChange(handleChecked(item.name))}
+                      defaultChecked={
+                        props.defaultChecked.includes(item.name) ? true : false
+                      }
+                    />
+                  }
+                  key={item.id}
+                  label={item.name}
+                  {...field}
+                />
+              ))
+            }
+          />
+        </FormControl>
+        {errors.modes ? (
+          <p
+            style={{
+              color: "red",
+              margin: "0px",
+              padding: "0px 0px 0px 10px",
+              fontSize: "14px",
+            }}
+          >
+            {errors.modes.type === "validate"
+              ? "Select Minimum Two Mode"
+              : null}
+          </p>
+        ) : null}
+      </Box>
+    </>
   );
 };
 
