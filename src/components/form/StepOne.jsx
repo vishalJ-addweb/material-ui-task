e0f88710a35cda41e85a3a214d0cec9eb8577a47
vishalJ-addweb <vishal.j@addwebsolution.in>
2022-01-13 18:16:59 +0530

    fix : validation on email

--- src/components/form/StepOne.jsx
+++ src/components/form/StepOne.jsx
@@ -91,6 +91,10 @@ const StepOne = (props) => {
             name="email"
             rules={{
               required: "Email is required.",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "invalid email address",
+              },
             }}
             render={({ field }) => (
               <TextField
